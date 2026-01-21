7e862c4a9d15f494a2f990a26048d390b3e2017d
Alex Kuzmin <alex.kuzmin@ethereum.org>
2026-01-21 16:13:40 +0800

    Minimize diff

--- whir/tests/pcs.rs
+++ whir/tests/pcs.rs
@@ -224,7 +224,6 @@ mod koala_bear_whir_pcs {
     type FieldHash = KeccakU32BeLeafHasher;
     type Compress = KeccakNodeCompress;
     type Dft<Val> = Radix2DitParallel<Val>;
-
     type Challenger = SerializingChallenger32<Val, HashChallenger<u8, ByteHash, 32>>;
     type MyPcs<Val, Dft> = WhirPcs<Val, Dft, FieldHash, Compress, 4>;
 
